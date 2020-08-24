2cc666ffad5a60877505f504bbe298b22aa2cba2
Kostiantyn Berehovyi <edifaer29122@gmail.com>
2020-08-25 00:10:12 +0300

    Updates

---
+++ precache-manifest.ea7fd330d40f7fa1022862cbb81b5dbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bc5d62759b7a480f14341ad56b9324",
+    "url": "/star-wars-db/index.html"
+  },
+  {
+    "revision": "77f1d0a5a1c5934e975e",
+    "url": "/star-wars-db/static/css/main.b5f215e8.chunk.css"
+  },
+  {
+    "revision": "69577e3e87dbc5e532c7",
+    "url": "/star-wars-db/static/js/2.e89bbfe4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-wars-db/static/js/2.e89bbfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f1d0a5a1c5934e975e",
+    "url": "/star-wars-db/static/js/main.7453441c.chunk.js"
+  },
+  {
+    "revision": "630f5d5f507e233bc1ec",
+    "url": "/star-wars-db/static/js/runtime-main.95ecfa1b.js"
+  },
+  {
+    "revision": "533696c0ec8ce19eb401162501f81e87",
+    "url": "/star-wars-db/static/media/death-star.533696c0.png"
+  }
+]);
